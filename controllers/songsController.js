61a2e5cb571f6717f499bed6a5afc59498c6ec91
xavierrice <xavier.rice@gmail.com>
2023-10-31 16:22:27 -0400

    added queries

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const songs = express.Router();
-
+const { getAllSongs } = require('../queries/songs.js')
 
 
 //INDEX OF ALL SONGS
-songs.get('/', (req, res) => {
-    res.json({status:"ok"})
-}) 
+songs.get('/', async (req, res) => {
+    const allSongs = await getAllSongs()
+    if(allSongs[0]){
+       res.status(200).json(allSongs) 
+    } else {
+        res.status(500).json({status:"server error"})
+    }
+});
 
 
 module.exports = songs;
