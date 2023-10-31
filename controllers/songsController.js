df033bfa6b4d9a840ba69a6170483e63117fbccf
xavierrice <xavier.rice@gmail.com>
2023-10-31 19:19:32 -0400

    added show route

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -12,6 +12,23 @@ songs.get('/', async (req, res) => {
         res.status(500).json({status:"server error"})
     }
 });
+//SHOW ID
+songs.get('/:id', (req, res) => {
+    try {
+        const id = req.params.id;
+        const targetSong = allSongs.find(
+            (song) => song.id === parseInt(id)
+        );
+        if (targetSong) {
+            console.log(targetSong)
+            res.status(200).json(targetSong)
+        }else {
+            res.status(404).json({message:"Couldn't find your Jam"})
+        }
+    } catch (error){
+        console.log(error)
+    }
+});
 
 
 module.exports = songs;
